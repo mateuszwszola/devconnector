abcbd36ae5c8199a58aae98e29cbe891c773a936
Mateusz Wszola <wszola.m@gmail.com>
2019-05-11 15:17:24 +0200

    Add router and handle initial register functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { Navbar, Landing } from './components/layout';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 import './App.css';
 
 const App = () =>
-  <Fragment>
-    <h1>App</h1>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route render={() => <div>Four oh four</div>} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+
+
+
+
 
 export default App;
