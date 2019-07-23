3e096430d323ae2dc971ccdb04b49ada202dfa2b
Mateusz Wszola <wszola.m@gmail.com>
2019-07-23 22:27:07 +0200

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 require('./config');
 const connectDB = require('./config/db');
 
@@ -12,12 +13,20 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => res.send('API is running'));
-
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
+
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
